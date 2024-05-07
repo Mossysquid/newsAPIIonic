3fa6f29925888effe3a1a3871721378a3444fcf0
Andrew Rasband <andrew.rasband@outlook.com>
2024-05-07 09:40:41 -0600

    removed empty route

--- newsAPI/src/app/app.routes.ts
+++ newsAPI/src/app/app.routes.ts
@@ -5,11 +5,7 @@ export const routes: Routes = [
     path: 'home',
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   },
-  {
-    path: 'message/:id',
-    loadComponent: () =>
-      import('./view-message/view-message.page').then((m) => m.ViewMessagePage),
-  },
+  
   {
     path: '',
     redirectTo: 'home',
