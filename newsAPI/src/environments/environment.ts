cf4a6e3db1ee0551dd1cdcde48ff459b9a4f9f51
Beck20003 <beckburnham03@gmail.com>
2024-05-09 08:25:37 -0600

    added firebase and created navbar component

--- newsAPI/src/environments/environment.ts
+++ newsAPI/src/environments/environment.ts
@@ -2,9 +2,22 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+// export const environment = {
+//   production: false,
+//   ApiKey: 'f2e1f7104cb34c8a8f79e0b1f97c3db5'
+// };
+
 export const environment = {
   production: false,
   ApiKey: 'f2e1f7104cb34c8a8f79e0b1f97c3db5'
+  firebase: {
+      apiKey: "AIzaSyBEaNcoARhaaugJ7B56awRpQteAQxhB09g",
+      authDomain: "angularfire-4879f.firebaseapp.com",
+      projectId: "angularfire-4879f",
+      storageBucket: "angularfire-4879f.appspot.com",
+      messagingSenderId: "394748093272",
+      appId: "1:394748093272:web:3c98685f961a8f7d34bb2d"
+  }
 };
 
 /*
