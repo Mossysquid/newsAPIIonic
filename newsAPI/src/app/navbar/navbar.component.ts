d32fb3b403476d14963992eb327582e72182e105
Beck20003 <beckburnham03@gmail.com>
2024-05-10 08:03:06 -0600

    added working navbar

--- newsAPI/src/app/navbar/navbar.component.ts
+++ newsAPI/src/app/navbar/navbar.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterLinkWithHref } from '@angular/router';
+import { IonProgressBar, IonItem, IonRow, IonCard, IonCol, IonGrid, IonContent, IonHeader, IonTitle, IonToolbar, IonList} from '@ionic/angular/standalone';
+
+
 
 @Component({
+  standalone: true,
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
+  imports: [IonProgressBar, IonItem, IonRow, IonCard, IonCol, IonGrid, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, RouterLinkWithHref, IonList]
 })
-export class NavbarComponent  implements OnInit {
+export class NavbarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {}
-
+  ngOnInit(): void {}
 }
