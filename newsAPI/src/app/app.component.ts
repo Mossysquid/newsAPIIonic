4c4a901a69a01637c9ec03d1d5b562c000437c17
Andrew Rasband <andrew.rasband@outlook.com>
2024-05-14 08:53:44 -0600

    added catagories filtration just need to hook up to query selector

--- newsAPI/src/app/app.component.ts
+++ newsAPI/src/app/app.component.ts
@@ -1,14 +1,24 @@
-import { IonMenu, IonMenuButton, IonMenuToggle } from '@ionic/angular/standalone';
+import { IonMenu, IonMenuButton, IonMenuToggle, IonCheckbox, IonLabel, SearchbarInputEventDetail } from '@ionic/angular/standalone';
 import { Component } from '@angular/core';
-import { } from '@ionic/angular';
 import { IonApp, IonRouterOutlet,  IonCard, IonHeader, IonItem, IonToolbar, IonTitle, IonContent, IonList, IonButtons, IonSplitPane } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
-  imports: [IonSplitPane, IonButtons, IonList, IonContent, IonTitle, IonToolbar, IonItem, IonHeader, IonApp, IonRouterOutlet, IonCard, IonMenu, IonMenuToggle, IonMenuButton],
+  imports: [IonLabel, IonCheckbox, IonSplitPane, IonButtons, IonList, IonContent, IonTitle, IonToolbar, IonItem, IonHeader, IonApp, IonRouterOutlet, IonCard, IonMenu, IonMenuToggle, IonMenuButton],
 })
 export class AppComponent {
   constructor() {}
+  selectedCatagories: string[] = [];handleCheckboxChange(label: string): void {
+    if (this.selectedCatagories.includes(label)) {
+      // Label is already selected, so remove it
+      this.selectedCatagories = this.selectedCatagories.filter(item => item !== label);
+    } else {
+      // Label is not selected, so add it
+      this.selectedCatagories.push(label);
+    }
+    console.log(this.selectedCatagories)
+  }
+
 }
